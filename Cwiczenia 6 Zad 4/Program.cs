9938e23d6e8f6a814a991d90edd12ac5d4025b71
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-28 14:45:50 +0100

    Add files via upload

---
+++ Cwiczenia 6 Zad 4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Cwiczenia_6_Zad_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst1 = "jakis       tam    tekst z    mnostwem    spacji";
+            string tekst2 = "";
+            int licznik = 0;
+
+            while (licznik < tekst1.Length-1)
+            {
+                if (tekst1[licznik] != ' ' | tekst1[licznik] == ' ' & tekst1[licznik+1] != ' ')
+                tekst2 += tekst1[licznik];
+                licznik++;
+
+            }
+            tekst2 += tekst1[tekst1.Length-1];
+            Console.WriteLine(tekst2);
+
+            
+        }
+    }
+}
